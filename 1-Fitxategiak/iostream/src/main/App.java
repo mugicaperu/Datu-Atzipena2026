c5120c0224e02a4ab9cd26c5f9639269d60042d0
mugicaperu <mugica.peru@uni.eus>
2026-09-17 10:33:31 +0200

    a

---
+++ 1-Fitxategiak/iostream/src/main/App.java
@@ -0,0 +1,6 @@
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Java project initialized successfully.");
+        System.out.println("Workspace ready to run.");
+    }
+}
